2c6208576bf367e652a10903d08389c4c13792fe
zataara <bryantmac@gmail.com>
2021-12-09 18:05:45 -0800

    added navbar for mvoing around app

---
+++ src/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {  NavLink } from "react-router-dom";
+
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="Navbar">
+      <NavLink exact to="/apples">
+        Apples
+      </NavLink>
+      <NavLink exact to="/oranges">
+        Oranges
+      </NavLink>
+      <NavLink exact to="/bananas">
+        Bananas
+      </NavLink>
+      <NavLink exact to="/blueberries">
+        Blueberries
+      </NavLink>
+      <NavLink exact to="/cherries">
+        Cherries
+      </NavLink>
+      <NavLink exact to="peaches">
+        Peaches
+      </NavLink>
+      <NavLink exact to="watermelon">
+        Watermelon
+      </NavLink>
+    </nav>
+  );
+};
+
+export default NavBar;
